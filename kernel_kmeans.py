43769b2296d5725472cb890a701ea6878673e8e0
bauxn <a01632415@unet.univie.ac.at>
2023-04-27 16:49:34 +0200

    Refactored _lloyd_iter for torch, cleaned up.

--- kernel_kmeans.py
+++ kernel_kmeans.py
@@ -4,10 +4,9 @@ from sklearn.utils import check_random_state
 
 
 class KKMeans():
-    def __init__(self, n_clusters = 8, init = "random", n_init = 1,
-                 max_iter = 300, tol = None, verbose = 0,
-                 random_state = None, algorithm = "lloyd", kernel = "linear",
-                 **kwds):
+    def __init__(self, n_clusters=8, init="random", n_init=1,
+                 max_iter=300, tol=None, verbose=0,
+                 random_state=None, algorithm="lloyd", kernel="linear", **kwds):
         self.n_clusters = n_clusters
         self.init = init
         self.n_init = n_init
@@ -20,54 +19,59 @@ class KKMeans():
         self.kwds = kwds
         self.labels = None
         return
-
+    
+    def p_kernel_wrapper(self, x, y=None):
+        return pairwise_kernels(x, y, metric=self.kernel, n_jobs=-1, **self.kwds)
+    
     def fit(self, data):
         self._check_data(data)
-
+        
         if self.algorithm == "lloyd":
             self._lloyd(data)
-
+        
     def _check_data(self, data):
         return
-
+    
     def _lloyd(self, data):
-        kernel_matrix = pairwise_kernels(X = data, metric = self.kernel,
-                                         **self.kwds)
+        kernel_matrix = self.p_kernel_wrapper(data)
         self._init(data, kernel_matrix)
-        distances = np.zeros((len(data), self.n_clusters))
-
+        inertia = 0
         for _ in range(self.max_iter):
-            distances.fill(0)
-            for cluster in range(self.n_clusters):
-                mask = (self.labels == cluster)
-                n_cluster_elements = sum(mask)   
-                if n_cluster_elements == 0:
-                    if self.verbose:
-                        print("Empty Cluster encountered in iteration",
-                              _, "Assigned random element to cluster.")
-                    self.labels[self.random_state.randint(len(self.labels))] = cluster
-                    n_cluster_elements = 1      
-                # (SUM K(a,b) for a,b in Cluster) / |cluster|**2 
-                inner_term = kernel_matrix[mask][:, mask].sum() / (n_cluster_elements ** 2)
-                # array that contains for each datapoint x: 2 * (SUM K(x,b) for b in Cluster) / |Cluster|
-                element_term = 2 * kernel_matrix[:, mask].sum(axis = 1) / n_cluster_elements
-                distances[:, cluster] += inner_term
-                distances[:, cluster] -= element_term
-            new_labels = np.argmin(distances, axis = 1)
-            if self.tol:
-                #todo
-                break
-            elif all(new_labels == self.labels):
+            distances = np.tile(np.diag(kernel_matrix), (self.n_clusters, 1)).T
+            self._lloyd_iter(kernel_matrix, distances)
+            labels_new = np.argmin(distances, axis=1)
+            inertia_new = np.amin(distances, axis=1).sum()
+            if all(labels_new == self.labels) or abs(inertia - inertia_new) < self.tol:
                 if self.verbose:
-                    print("Converged at iteration: ", _ + 1)
-                break
-            self.labels = new_labels
-        return
-
+                    print("Converged at iteration:", _ + 1,
+                          "Inertia:", inertia_new)
+                break                
+            if self.verbose:
+                print("Iteration:", _ + 1,
+                      "Inertia:", inertia_new)
+            self.labels = labels_new
+            inertia = inertia_new
+    
+    def _lloyd_iter(self, kernel_matrix, distances):
+        for cluster in range(self.n_clusters):
+            mask = (self.labels == cluster)
+            n_cluster_elements = sum(mask) 
+            if n_cluster_elements == 0:
+                if self.verbose:
+                    print("Empty Cluster encountered,",  
+                          "assigned random element to cluster.")
+                self.labels[self.random_state.randint(len(self.labels))] = cluster
+                n_cluster_elements = 1      
+            # (SUM K(a,b) for a,b in Cluster) / |cluster|**2 
+            inner_term = kernel_matrix[mask][:, mask].sum() / (n_cluster_elements ** 2)
+            # array that contains for each datapoint x: 2 * (SUM K(x,b) for b in Cluster) / |Cluster|
+            element_term = 2 * kernel_matrix[:, mask].sum(axis = 1) / n_cluster_elements
+            distances[:, cluster] += inner_term
+            distances[:, cluster] -= element_term
+    
     def _init(self, data, kernel_matrix):
-
         if isinstance(self.init, (list, tuple, np.ndarray)):
-            self._check_centroids() # TODO ? zumindest len(self.init) == self.n_clusters
+            self._check_centroids() # TODO 
             self._assign_to_centroids(self.init, data)
             return
         
@@ -85,18 +89,18 @@ class KKMeans():
             return 
         
         raise Exception("Unknown initialisation method")
-            
+    
     def _check_centroids(self):
         if len(self.init) != self.n_clusters:
             raise Exception("The number of given centroids should match n_clusters") # TODO
         return
 
     def _assign_to_centroids(self, centroids, data):
-        data_centr_kernel = pairwise_kernels(X = data, Y = centroids, metric = self.kernel, **self.kwds)
+        data_centr_kernel = self.p_kernel_wrapper(data, centroids)
         centr_distances = np.zeros((len(data), self.n_clusters))
         for cluster in range(self.n_clusters):
             centr_distances[:, cluster] = (-2 * data_centr_kernel[:, cluster]
-                                            + pairwise_kernels([centroids[cluster]], metric = self.kernel, **self.kwds)) 
+                + self.p_kernel_wrapper([centroids[cluster]]))
         self.labels = np.argmin(centr_distances, axis = 1)
         return
 
@@ -110,15 +114,17 @@ class KKMeans():
                 #random first center
                 index = self.random_state.randint(len(data))
             else:
-                max_dist_each = np.amax(centr_distances, axis = 1)
-                index = self.random_state.choice(len(data), size = 1, p = max_dist_each/max_dist_each.sum())
-            #TODO testen!!
+                max_dist_each = np.amin(centr_distances[:, :cluster + 1], axis = 1)
+                max_dist_each[max_dist_each < 0] = 0
+                probs = max_dist_each/max_dist_each.sum()
+                index = self.random_state.choice(len(data), size = 1, p = probs)
             centroids[cluster] = data[index]
-            cluster_term = pairwise_kernels([centroids[cluster]], metric = self.kernel, **self.kwds)
-            data_term = pairwise_kernels(data, [centroids[cluster]], metric = self.kernel, **self.kwds)
-            centr_distances[:, cluster] += (-2 * data_term + cluster_term).reshape(len(data),)
-            
+            cluster_term = self.p_kernel_wrapper([centroids[cluster]])
+            data_term = self.p_kernel_wrapper(data, [centroids[cluster]])
+            centr_distances[:, cluster] += (-2 * data_term + cluster_term).reshape(len(data),)    
         self.labels = np.argmin(centr_distances, axis = 1)
+                
         return
-    
-
+            
+            
+            
