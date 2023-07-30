8a056ae70094aee1d8fe8b6bbe68aa9120e6541b
bauxn <a01632415@unet.univie.ac.at>
2023-07-30 22:03:21 +0200

    small change to setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-# from distutils.ccompiler import new_compiler  # used to determine command line argument for openmMP 
 
 
-# print("HELLO")
+# # default
+compile_args = ["-DCYTHON_WITHOUT_ASSERTIONS"]
+
+# # msvc
+# compile_args = ["/O2", "-DCYTHON_WITHOUT_ASSERTIONS", "/openmp"]
+
 # # gcc
 # compile_args = ["-O3", "-ffast-math", "-DCYTHON_WITHOUT_ASSERTIONS", "-fopenmp"]
-
-# ccompiler = new_compiler()
-# if ccompiler.compiler_type == "msvc":
-#     compile_args = ["/O2", "-DCYTHON_WITHOUT_ASSERTIONS", "/openmp"]
-
-# if ccompiler.compiler_type not in ("gcc", "g++", "msvc"):
-#     print("unknown compiler used! Please provide compile args with\
-#           -C or --cargs. At least option to enable openmp heavily \
-#           recommended.")
-
-# compile_args = ["-DCYTHON_WITHOUT_ASSERTIONS"]
-compile_args = ["/O2", "-DCYTHON_WITHOUT_ASSERTIONS", "/openmp"]
 
 compiler_directives = {
     "language_level": 3, 
